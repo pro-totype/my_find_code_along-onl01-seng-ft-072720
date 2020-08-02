a92781d1ccd3d5344cf6ff2287f21c8779312c5a
Sean B <butts.sean01@gmail.com>
2020-08-02 02:03:59 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,11 @@
 require 'pry'
+install pry gem
 
 def my_find(collection)
   i = 0 
   while i < collection.length 
     if yield(collection[i])
+      binding.pry
       return collection[i]
     end
     i = i + 1
