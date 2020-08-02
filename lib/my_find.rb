28e4aad59c0a94d278deb12afa51120babf280c9
Sean B <butts.sean01@gmail.com>
2020-08-02 02:31:16 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -9,14 +9,14 @@ def my_find(collection)
   end
 end
 
-I am trying to get in the habit of knowing how to write the syntax of what is happening in the code. So I wanted to know if this is an accurate translation.
+#I am trying to get in the habit of knowing how to write the syntax of what is happening in the code. So I wanted to know if this is an accurate translation.
 
----
+#---
 
-For definition my_find(collection)
-the initial standard for i is equal to 0
-while i is less than collection length,
-loop this part of the program, 
-but if i=0 
-return the value collection[i]
-before looping and returning i= i + 1
+#For definition my_find(collection)
+#the initial standard for i is equal to 0
+#while i is less than collection length,
+#loop this part of the program, 
+#but if i=0 
+#return the value collection[i]
+#before looping and returning i= i + 1
